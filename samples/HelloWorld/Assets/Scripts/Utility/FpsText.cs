df5cddfdb33e240c5694dc5ae3b99f3dbf74592e
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2022-11-03 19:20:27 -0700

    Added FpsText.cs utility for controlling fps meter.
    PiperOrigin-RevId: 486032228

---
+++ samples/HelloWorld/Assets/Scripts/Utility/FpsText.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GoogleMobileAds.Samples.Utility
+{
+    /// <summary>
+    /// Frames per second text element.
+    /// </summary>
+    [AddComponentMenu("GoogleMobileAds/Samples/Utility/FpsText")]
+    public class FpsText : Text
+    {
+        private void Update()
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            text = string.Format("{0:0.} fps", fps);
+        }
+    }
+}
